420b5176dff35c8e4219605caf2b889f2188611e
deverloperbillu <syedbilalshahid120@gmail.com>
2025-07-17 22:59:58 +0500

    Minnor Changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['dev-nextwebstore.pantheonsite.io/']
+    domains: ['dev-nextwebstore.pantheonsite.io']
   },
 };
 export default nextConfig;
